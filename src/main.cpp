f6cfc5ec20d5083c9d48d09c624ab4f9d8951c07
timonent <tatu.timonen@gmail.com>
2020-11-29 02:27:42 +0200

    Add some shader classes

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 
+#include "Shader.hpp"
+#include "ShaderProgram.hpp"
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
+#include <SOIL.h>
 
 #include <omp.h>
 
@@ -16,98 +20,71 @@ int main(void)
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-    auto window = glfwCreateWindow(800, 600, "Hello there.", nullptr, nullptr);
+    auto window = glfwCreateWindow(800, 800, "Hello there.", nullptr, nullptr);
     glfwMakeContextCurrent(window);
     glfwSetKeyCallback(window, key_callback);
     glewExperimental = GL_TRUE;
     glewInit();
 
-    glViewport(0, 0, 800, 600);
+    glViewport(0, 0, 800, 800);
 
     GLfloat vertices[] = {
-        -0.5f, -0.5f,  0.0f,
-         0.0f,  0.5f,  0.0f,
-         0.5f, -0.5f,  0.0f,
-         0.0f,  0.0f,  0.0f,
-         0.5f,  1.0f,  0.0f,
-         1.0f,  0.0f,  0.0f 
+        -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+         0.5f,  0.5f,  0.0f,  1.0f,  1.0f,
+         0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+        -0.5f, -0.5f,  0.0f,  0.0f,  0.0f
     };
+    GLuint indices[] = {
+        0, 1, 2,
+        2, 3, 0
+    };
+    // Vertex array object holds vertex attribute data, including possible element data.
     GLuint vao;
     glGenVertexArrays(1, &vao);
     GLuint vbo;
     glGenBuffers(1, &vbo);
+    GLuint ebo;
+    glGenBuffers(1, &ebo);
+
     glBindVertexArray(vao);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
-    glEnableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    {
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+        // Vertex positions.
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5*sizeof(GLfloat), (GLvoid*)0);
+        glEnableVertexAttribArray(0);
+        // Texture coords.
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5*sizeof(GLfloat), (GLvoid*)(3*sizeof(GLfloat)));
+        glEnableVertexAttribArray(1);
+    }
     glBindVertexArray(0);
 
-    GLuint vertex_shader;
-    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-    const GLchar* vertex_shader_source = "\
-        #version 330\n\
-        \n\
-        layout (location = 0) in vec3 position;\n\
-        \n\
-        out vec3 position_out;\n\
-        \n\
-        void main()\n\
-        {\n\
-            gl_Position = vec4(position.x, position.y, position.z, 1.0);\n\
-            position_out = gl_Position.xyz;\n\
-        }\n\
-    ";
-    glShaderSource(vertex_shader, 1, &vertex_shader_source, nullptr);
-    glCompileShader(vertex_shader);
+    GLuint tex;
+    glGenTextures(1, &tex);
+    glBindTexture(GL_TEXTURE_2D, tex);
 
-    GLuint fragment_shader;
-    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-    const GLchar* fragment_shader_source = "\
-        #version 330 core\n\
-        \n\
-        in vec3 position_out;\n\
-        out vec4 color;\n\
-        \n\
-        void main()\n\
-        {\n\
-            color = vec4(position_out, 1.0f);\n\
-        }\n\
-    ";
-    glShaderSource(fragment_shader, 1, &fragment_shader_source, nullptr);
-    glCompileShader(fragment_shader);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-    GLuint shader_program;
-    shader_program = glCreateProgram();
-    glAttachShader(shader_program, vertex_shader);
-    glAttachShader(shader_program, fragment_shader);
-    glLinkProgram(shader_program);
+    int w, h;
+    unsigned char* img = SOIL_load_image("../texture/cataphract.jpg", &w, &h, 0, SOIL_LOAD_RGB);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, w, h, 0, GL_RGB, GL_UNSIGNED_BYTE, img);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    SOIL_free_image_data(img);
+    glBindTexture(GL_TEXTURE_2D, 0);
 
-    GLuint fragment_shader_2;
-    fragment_shader_2 = glCreateShader(GL_FRAGMENT_SHADER);
-    const GLchar* fragment_shader_source_2 = "\
-        #version 330 core\n\
-        \n\
-        out vec4 color;\n\
-        \n\
-        uniform vec4 u_color;\n\
-        \n\
-        void main()\n\
-        {\n\
-            color = u_color;\n\
-        }\n\
-    ";
-    glShaderSource(fragment_shader_2, 1, &fragment_shader_source_2, nullptr);
-    glCompileShader(fragment_shader_2);
+    Shader vertex_shader = Shader("../src/vshader.glsl", GL_VERTEX_SHADER);
+    Shader fragment_shader = Shader("../src/fshader.glsl", GL_FRAGMENT_SHADER);
+    ShaderProgram shader_program = ShaderProgram(vertex_shader, fragment_shader);
+    shader_program.use();
 
-    GLuint shader_program_2;
-    shader_program_2 = glCreateProgram();
-    glAttachShader(shader_program_2, vertex_shader);
-    glAttachShader(shader_program_2, fragment_shader_2);
-    glLinkProgram(shader_program_2);
-    glDeleteShader(vertex_shader);
-    glDeleteShader(fragment_shader);
+    glBindVertexArray(vao);
 
     while (!glfwWindowShouldClose(window)) {
         
@@ -116,21 +93,17 @@ int main(void)
         glClearColor(0.4627f, 0.7255f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBindVertexArray(vao);
-        glUseProgram(shader_program);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
-        GLfloat time = glfwGetTime();
-        GLfloat green_shade = (glm::sin(time) / 2) + 0.5;
-        GLint color_location = glGetUniformLocation(shader_program_2, "u_color");
-        glUseProgram(shader_program_2);
-        glUniform4f(color_location, 0.0f, 0.0f, green_shade, 1.0f);
-        glDrawArrays(GL_TRIANGLES, 3, 3);
-        glBindVertexArray(0);
+        glBindTexture(GL_TEXTURE_2D, tex);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
 
     }
+
+    glBindVertexArray(0);
+    glDeleteVertexArrays(1, &vao);
+    glDeleteBuffers(1, &vbo);
+    glDeleteBuffers(1, &ebo);
 
     glfwTerminate();
 
--- src/vshader.glsl
+++ src/vshader.glsl
@@ -1,8 +1,12 @@
 #version 330
 
-layout(location = 0) in vec3 pos;
+layout (location = 0) in vec3 position;
+layout (location = 1) in vec2 tex_coord;
 
-void main(void)
+out vec2 tex_coord_out;
+
+void main()
 {
-    gl_Position = vec4(pos, 1.0f);
+    gl_Position = vec4(position, 1.0);
+    tex_coord_out = vec2(tex_coord.x, 1.0f - tex_coord.y);
 }
