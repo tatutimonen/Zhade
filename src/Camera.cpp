6f91678ccb882ec943d7d064dbd20aebd9d1d22f
timonent <tatu.timonen@gmail.com>
2021-03-23 16:34:02 +0200

    Add specification for camera

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -1,35 +1,31 @@
 #include "Camera.hpp"
 
-Camera::Camera(const glm::vec3& position,
-               const glm::vec3& target,
-               const glm::vec3& up,
-               float z_near,
-               float z_far)
-    : m_position(position), m_target(target), m_up(up), m_z_near(z_near), m_z_far(z_far)
+Camera::Camera(std::unique_ptr<Specification> spec)
+    : m_spec(std::move(spec))
 {
     set_view();
 }
 
 bool Camera::move()
 {
-    const glm::vec3 position_prev = m_position;
+    const glm::vec3 position_prev = m_spec->position;
     const auto keys = App::get_instance().get_keys();
     const float camera_speed = s_camera_base_speed * App::get_instance().get_delta_time();
     
     if (keys[GLFW_KEY_W])
-        m_position += camera_speed * m_target;
+        m_spec->position += camera_speed * m_spec->target;
     if (keys[GLFW_KEY_S]) 
-        m_position += camera_speed * -m_target;
+        m_spec->position += camera_speed * -m_spec->target;
     if (keys[GLFW_KEY_D])
-        m_position += camera_speed * glm::normalize(glm::cross(m_target, m_up));
+        m_spec->position += camera_speed * glm::normalize(glm::cross(m_spec->target, m_spec->up));
     if (keys[GLFW_KEY_A])
-        m_position += camera_speed * -glm::normalize(glm::cross(m_target, m_up));
+        m_spec->position += camera_speed * -glm::normalize(glm::cross(m_spec->target, m_spec->up));
     if (keys[GLFW_KEY_SPACE])
-        m_position += camera_speed * Util::make_unit_vec3y();
+        m_spec->position += camera_speed * Util::make_unit_vec3y();
     if (keys[GLFW_KEY_LEFT_SHIFT])
-        m_position += camera_speed * -Util::make_unit_vec3y();
+        m_spec->position += camera_speed * -Util::make_unit_vec3y();
     
-    if (m_position != position_prev) {
+    if (m_spec->position != position_prev) {
         set_view();
         set_projectivity();
         return true;
@@ -42,13 +38,13 @@ bool Camera::rotate()
 {
     const float pitch = App::get_instance().get_pitch();
     const float yaw = App::get_instance().get_yaw();
-    const glm::vec3 target_prev = m_target;
-    m_target.x = glm::cos(pitch) * glm::cos(yaw);
-    m_target.y = glm::sin(pitch);
-    m_target.z = glm::cos(pitch) * glm::sin(yaw);
-    m_target = glm::normalize(m_target);
+    const glm::vec3 target_prev = m_spec->target;
+    m_spec->target.x = glm::cos(pitch) * glm::cos(yaw);
+    m_spec->target.y = glm::sin(pitch);
+    m_spec->target.z = glm::cos(pitch) * glm::sin(yaw);
+    m_spec->target = glm::normalize(m_spec->target);
 
-    if (m_target != target_prev) {
+    if (m_spec->target != target_prev) {
         set_view();
         set_projectivity();
         return true;
@@ -68,41 +64,27 @@ void Camera::push_projection_matrix(ShaderProgram& shader_program)
 }
 
 
-OrthographicCamera::OrthographicCamera(const glm::vec3& position,
-                                       const glm::vec3& target,
-                                       const glm::vec3& up,
-                                       float z_near,
-                                       float z_far,
-                                       float x0,
-                                       float x1,
-                                       float y0,
-                                       float y1)
-    : Camera(position, target, up, z_near, z_far),
-      m_x_range(glm::vec2(x0, x1)), m_y_range(glm::vec2(y0, y1))
+OrthographicCamera::OrthographicCamera(std::unique_ptr<Specification> spec)
+    : Camera(std::move(spec))
 {
     set_projectivity();
 }
 
 void OrthographicCamera::set_projectivity()
 {
-    m_projectivity = glm::ortho(m_x_range[0], m_x_range[1], m_y_range[0], m_y_range[1], m_z_near, m_z_far);
+    auto spec = dynamic_cast<OrthographicCamera::Specification*>(m_spec.get());
+    m_projectivity = glm::ortho(spec->x_range[0], spec->x_range[1], spec->y_range[0], spec->y_range[1], spec->z_near, spec->z_far);
 }
 
 
-PerspectiveCamera::PerspectiveCamera(const glm::vec3& position,
-                                     const glm::vec3& target,
-                                     const glm::vec3& up,
-                                     float z_near,
-                                     float z_far,
-                                     float fov,
-                                     float aspect_ratio)
-    : Camera(position, target, up, z_near, z_far),
-      m_fov(fov), m_aspect_ratio(aspect_ratio)
+PerspectiveCamera::PerspectiveCamera(std::unique_ptr<Specification> spec)
+    : Camera(std::move(spec))
 {
     set_projectivity();
 }
 
 void PerspectiveCamera::set_projectivity()
 {
-    m_projectivity = glm::perspective(m_fov, m_aspect_ratio, m_z_near, m_z_far);
+    auto spec = dynamic_cast<PerspectiveCamera::Specification*>(m_spec.get());
+    m_projectivity = glm::perspective(spec->fov, spec->aspect_ratio, spec->z_near, spec->z_far);
 }
--- src/Camera.hpp
+++ src/Camera.hpp
@@ -8,25 +8,37 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
+#include <memory>
+
 
 class Camera {
 public:
-    Camera();
+    struct Specification {
+        glm::vec3 position;
+        glm::vec3 target;
+        glm::vec3 up;
+        float z_near;
+        float z_far;
+
+        Specification(const glm::vec3& position = glm::vec3(),
+                      const glm::vec3& target   = glm::vec3(0.0f, 0.0f, -1.0f),
+                      const glm::vec3& up       = glm::vec3(0.0f, 1.0f, 0.0f),
+                      float z_near              = 0.1f,
+                      float z_far               = 100.0f)
+            : position(position), target(target), up(up), z_near(z_near), z_far(z_far) {}
+        virtual ~Specification() = default;
+    };
+
     virtual ~Camera() = default;
 
-    inline void set_position(const glm::vec3& position) { m_position = position; }
-    inline void set_target(const glm::vec3& target)     { m_target = target; }
-    inline void set_up(const glm::vec3& up)             { m_up = up; }
-    inline void set_view()                              { m_view = glm::lookAt(m_position, m_position + m_target, m_up); }
+    inline void set_view() { m_view = glm::lookAt(m_spec->position,
+                                                  m_spec->position + m_spec->target,
+                                                  m_spec->up); }
     virtual void set_projectivity() = 0;
 
-    inline const glm::vec3& get_position() const     { return m_position; }
-    inline const glm::vec3& get_target() const       { return m_target; }
-    inline const glm::vec3& get_up() const           { return m_up; }
-    inline float get_z_near() const                  { return m_z_near; }
-    inline float get_z_far() const                   { return m_z_far; }
     inline const glm::mat4& get_view() const         { return m_view; }
     inline const glm::mat4& get_projectivity() const { return m_projectivity; }
+    virtual const Specification* get_spec() const    = 0;
 
     bool move();
     bool rotate();
@@ -36,63 +48,53 @@ public:
     static constexpr float s_camera_base_speed = 2.5f;
 
 protected:
-    Camera(const glm::vec3& position,
-           const glm::vec3& target,
-           const glm::vec3& up,
-           float z_near,
-           float z_far);
+    Camera(std::unique_ptr<Specification> base_spec);
 
-    glm::vec3 m_position;
-    glm::vec3 m_target;
-    glm::vec3 m_up;
-    float m_z_near;
-    float m_z_far;
-    glm::mat4 m_view = glm::mat4(1.0f);
+    std::unique_ptr<Specification> m_spec;
+    glm::mat4 m_view         = glm::mat4(1.0f);
     glm::mat4 m_projectivity = glm::mat4(1.0f);
 };
 
 
 class OrthographicCamera final : public Camera {
 public:
-    OrthographicCamera(const glm::vec3& position = glm::vec3(),
-                       const glm::vec3& target   = glm::vec3(0.0f, 0.0f, -1.0f),
-                       const glm::vec3& up       = glm::vec3(0.0f, 1.0f, 0.0f),
-                       float z_near              =  0.1f,
-                       float z_far               =  100.0f,
-                       float x0                  = -5.0f,
-                       float x1                  =  5.0f,
-                       float y0                  = -5.0f,
-                       float y1                  =  5.0f);
+    struct Specification : Camera::Specification {
+        glm::vec2 x_range;
+        glm::vec2 y_range;
+
+        Specification(std::unique_ptr<Camera::Specification> base_spec = std::make_unique<Camera::Specification>(),
+                      const glm::vec2& x_range                         = glm::vec2(-5.f, 5.f),
+                      const glm::vec2& y_range                         = glm::vec2(-5.f, 5.f))
+            : Camera::Specification(*base_spec.get()),
+              x_range(x_range), y_range(y_range) {}
+    };
+
+    OrthographicCamera(std::unique_ptr<Specification> spec = std::make_unique<Specification>());
     ~OrthographicCamera() = default;
 
-    void set_projectivity() override;
+    virtual void set_projectivity() override;
 
-    inline const glm::vec2& get_x_range() const { return m_x_range; }
-    inline const glm::vec2& get_y_range() const { return m_y_range; }
-
-private:
-    glm::vec2 m_x_range;
-    glm::vec2 m_y_range;
+    virtual const Specification* get_spec() const { return dynamic_cast<Specification*>(m_spec.get()); }
 };
 
 
 class PerspectiveCamera final : public Camera {
 public:
-    PerspectiveCamera(const glm::vec3& position = glm::vec3(),
-                      const glm::vec3& target   = glm::vec3(0.0f, 0.0f, -1.0f),
-                      const glm::vec3& up       = glm::vec3(0.0f, 1.0f, 0.0f),
-                      float z_near              = 0.1f,
-                      float z_far               = 100.0f,
-                      float fov                 = 70.0f,
-                      float aspect_ratio        = (float)App::window_width / (float)App::window_height);
+    struct Specification : Camera::Specification {
+        float fov;
+        float aspect_ratio;
+
+        Specification(std::unique_ptr<Camera::Specification> base_spec = std::make_unique<Camera::Specification>(),
+                      float fov                                        = 70.f,
+                      float aspect_ratio                               = (float)App::window_width / (float)App::window_height)
+            : Camera::Specification(*base_spec.get()),
+              fov(fov), aspect_ratio(aspect_ratio) {}
+    };
+
+    PerspectiveCamera(std::unique_ptr<Specification> spec = std::make_unique<Specification>());
     ~PerspectiveCamera() = default;
 
-    void set_projectivity() override;
+    virtual void set_projectivity() override;
 
-    inline float get_fov() const          { return m_fov; }
-    inline float get_aspect_ratio() const { return m_aspect_ratio; }
-
-private:
-    float m_fov;
-    float m_aspect_ratio;
+    virtual const Specification* get_spec() const { return dynamic_cast<Specification*>(m_spec.get()); }
 };
