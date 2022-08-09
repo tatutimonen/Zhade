32db9f4cb9b13eca28b3ed140b3061bc254b453c
timonent <tatu.timonen@gmail.com>
2022-08-09 03:52:49 +0300

    Update stb_image wrapper

--- src/util.hpp
+++ src/util.hpp
@@ -49,7 +49,7 @@ public:
     StbImageResource(const T* data, const int size)
     {
         stbi_set_flip_vertically_on_load(1);
-        m_data = loadFromMemory(data, len);
+        m_data = loadFromMemory(data, size);
     }
 
     ~StbImageResource()
